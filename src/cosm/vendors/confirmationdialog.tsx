68ea85e21668f2ca95d5831785cc0e9180954e34
Lumivient <lumivient@gmail.com>
2026-09-11 10:08:56 +0100

    fmt

--- src/cosm/vendors/confirmationdialog.tsx
+++ src/cosm/vendors/confirmationdialog.tsx
@@ -45,18 +45,30 @@ export const ConfirmationDialog: React.FC<ConfirmationDialogProps> = ({
 					borderRadius: 2,
 					bgcolor: "background.default",
 					backgroundImage: "none",
-					padding: "1.6em"
+					padding: "1.6em",
 				},
 			},
 		}}
 	>
-		<DialogTitle sx={{ fontWeight: "bold", padding: 0, marginBottom: "1em" }}>{title}</DialogTitle>
+		<DialogTitle sx={{ fontWeight: "bold", padding: 0, marginBottom: "1em" }}>
+			{title}
+		</DialogTitle>
 		<DialogContent sx={{ padding: 0 }}>{children}</DialogContent>
 		<DialogActions sx={{ padding: 0, marginTop: "1.6em" }}>
-			<Button onClick={onClose} variant="outlined" color="inherit" sx={{ fontWeight: "bold" }}>
+			<Button
+				onClick={onClose}
+				variant="outlined"
+				color="inherit"
+				sx={{ fontWeight: "bold" }}
+			>
 				{cancelLabel}
 			</Button>
-			<Button onClick={onConfirm} variant="contained" color={actionColors[type]} sx={{ fontWeight: "bold" }}>
+			<Button
+				onClick={onConfirm}
+				variant="contained"
+				color={actionColors[type]}
+				sx={{ fontWeight: "bold" }}
+			>
 				{confirmLabel}
 			</Button>
 		</DialogActions>
